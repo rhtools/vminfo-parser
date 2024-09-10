b39b31261bd054bef2884e57c4738003aa300b44
Jason Ross <jaross@redhat.com>
2024-09-10 15:08:06 -0500

    initial rewrite of add extra columns

--- vminfo_parser/const.py
+++ vminfo_parser/const.py
@@ -37,3 +37,25 @@ MIME = MappingProxyType(
         "csv": "text/csv",
     }
 )
+
+EXTRA_COLUMNS_DEST = ["OS Name", "OS Version", "Architecture"]
+
+EXTRA_WINDOWS_SERVER_COLUMNS = [f"SERVER ${col}" for col in EXTRA_COLUMNS_DEST]
+EXTRA_WINDOWS_DESKTOP_COLUMNS = [f"DESKTOP ${col}" for col in EXTRA_COLUMNS_DEST]
+
+EXTRA_COLUMNS_NON_WINDOWS_REGEX = (
+    r"^(?!.*Microsoft)(?P<OS_Name>.*?)(?:\s+"
+    r"(?P<OS_Version>\d+(?:/\d+)*\s*(?:or later)?\s*)?\s*"
+    r"\((?P<Architecture>.*?64-bit|.*?32-bit)\))"
+)
+
+EXTRA_COLUMNS_WINDOWS_SERVER_REGEX = (
+    r"^(?P<OS_Name>Microsoft Windows Server)\s+"
+    r"(?P<OS_Version>\d+(?:\.\d+)*(?:\s*R\d+)?(?:\s*SP\d+)?)"
+    r"(?:\s*\((?P<Architecture>.*?64-bit|.*?32-bit)\))?"
+)
+EXTRA_COLUMNS_WINDOWS_DESKTOP_REGEX = (
+    r"^(?P<OS_Name>Microsoft Windows)\s+(?!Server)"
+    r"(?P<OS_Version>XP Professional|\d+(?:\.\d+)*|Vista|7|8|10)\s*"
+    r"\((?P<Architecture>.*?64-bit|.*?32-bit)\)"
+)
