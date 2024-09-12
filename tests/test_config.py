3545bc74a43b3dd18ed5ad3b0de2b11d93590d3d
Jason Ross <jaross@redhat.com>
2024-09-12 17:30:18 -0500

    Move logging prints to logging module.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 import sys
 
@@ -14,17 +15,17 @@ def test_yaml_load(config_dict: dict, yaml_config: str) -> None:
     assert_config_correct(config_dict, config_obj)
 
 
-def test_yaml_load_file_not_found(tmp_path: pathlib.Path, capsys: pytest.CaptureFixture) -> None:
+def test_yaml_load_file_not_found(tmp_path: pathlib.Path, caplog: pytest.LogCaptureFixture) -> None:
     yamlfile = str(tmp_path / "config.yaml")
     config_obj = Config(yaml=yamlfile)
 
     with pytest.raises(SystemExit):
         config_obj._load_yaml()
-    output = capsys.readouterr()
-    assert output.out == f"YAML file not found: {yamlfile}\n"
+
+    assert caplog.record_tuples == [("vminfo_parser.config", logging.CRITICAL, "YAML file not found: %s" % yamlfile)]
 
 
-def test_yaml_load_invalid_file(tmp_path: pathlib.Path, capsys: pytest.CaptureFixture) -> None:
+def test_yaml_load_invalid_file(tmp_path: pathlib.Path, caplog: pytest.LogCaptureFixture) -> None:
     yamlfile = str(tmp_path / "config.yaml")
     with open(yamlfile, "w") as file:
         file.write("value: !invalid\n")
@@ -32,37 +33,43 @@ def test_yaml_load_invalid_file(tmp_path: pathlib.Path, capsys: pytest.CaptureFi
 
     with pytest.raises(SystemExit):
         config_obj._load_yaml()
-    output = capsys.readouterr()
-    assert "Error parsing YAML file:" in output.out
+
+    assert caplog.record_tuples == [("vminfo_parser.config", logging.CRITICAL, "Error parsing YAML file")]
 
 
-def test_yaml_load_duplicate(tmp_path: pathlib.Path, capsys: pytest.CaptureFixture) -> None:
+def test_yaml_load_duplicate(tmp_path: pathlib.Path, caplog: pytest.LogCaptureFixture) -> None:
     yamlfile = str(tmp_path / "config.yaml")
     with open(yamlfile, "w") as file:
         file.write("test-value: test")
     config_obj = Config(yaml=yamlfile, test_value="original")
     config_obj._load_yaml()
 
-    output = capsys.readouterr()
     assert config_obj.test_value == "original"
-    assert output.out == "Ignoring test_value from yaml, already set.\n"
+    assert caplog.record_tuples == [
+        ("vminfo_parser.config", logging.WARNING, "Ignoring test_value from yaml, already set.")
+    ]
 
 
-def test_validate(capsys: pytest.CaptureFixture) -> None:
+def test_validate(caplog: pytest.LogCaptureFixture) -> None:
     config_obj = Config()
 
     with pytest.raises(SystemExit):
         config_obj._validate()
-    output = capsys.readouterr()
-    assert output.out == "File not specified in yaml or command line\n"
+
+    assert caplog.record_tuples == [
+        ("vminfo_parser.config", logging.CRITICAL, "File not specified in yaml or command line")
+    ]
 
 
-def test_cli_yaml_and_args(capsys: pytest.CaptureFixture) -> None:
+def test_cli_yaml_and_args(capsys: pytest.CaptureFixture, caplog: pytest.LogCaptureFixture) -> None:
     with pytest.raises(SystemExit):
         Config.from_args("--yaml", "filename", "--os-name", "osname")
     output = capsys.readouterr()
-    assert "When using --yaml, no other arguments should be provided.\n" in output.out
-    assert "usage:" in output.out
+    assert caplog.record_tuples == [
+        ("vminfo_parser.config", logging.ERROR, "When using --yaml, no other arguments should be provided.")
+    ]
+    assert "usage:" in output.err
+    assert "[-h] (--file FILE | --yaml YAML)" in output.err
 
 
 def test_yaml_from_args(config_dict: dict, yaml_config: str) -> None:
