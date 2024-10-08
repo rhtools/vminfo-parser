92e6e6cb33d9dc286b46e7724985c1ad2950bd6a
stratus-ss <steve.ovens@x86innovations.com>
2024-10-08 09:53:10 -0500

    added a test for format_rows()

--- tests/test_clioutput.py
+++ tests/test_clioutput.py
@@ -1,5 +1,6 @@
 import typing as t
 
+import pandas as pd
 import pytest
 
 from vminfo_parser.clioutput import CLIOutput
@@ -23,3 +24,46 @@ def test_write(cli_output: CLIOutput, arg: t.Any) -> None:
 def test_writeline(cli_output: CLIOutput, arg: t.Any) -> None:
     cli_output.writeline(arg)
     assert cli_output.output.getvalue() == f"{arg}" if str(arg).endswith("\n") else f"{arg}\n"
+
+
+@pytest.mark.parametrize(
+    "df, formatted_rows, justification, col_widths, index_column_name, expected",
+    [
+        (
+            pd.DataFrame({"Disk Space Range": ["0 - 200 GB", "1 - 2 TB"], "non-prod": [3, 4], "prod": [5, 6]}),
+            [],
+            15,
+            {"Disk Space Range": 22, "non-prod": 10, "prod": 10},
+            "Disk Space Range",
+            "0 - 200 GB                             3          5         \n"
+            "1 - 2 TB                               4          6         ",
+        ),
+        (
+            pd.DataFrame(
+                {"OS Version": ["7", "9"], "Disk Space Range": ["10 - 20 TB", "801 GB - 1 TB"], "Count": [15, 26]}
+            ),
+            [],
+            15,
+            {"OS Version": 0, "Disk Space Range": 19, "Count": 19},
+            "OS Version",
+            "7                10 - 20 TB          15                 \n"
+            "9                801 GB - 1 TB       26                 ",
+        ),
+    ],
+)
+def test_format_rows(
+    cli_output: CLIOutput,
+    df: pd.DataFrame,
+    formatted_rows: list,
+    justification: int,
+    col_widths: dict,
+    index_column_name: str,
+    expected: str,
+):
+    # Depending on what is being formatted, the index of the dataframe is different
+    df = df.set_index(f"{index_column_name}")
+    result = cli_output.format_rows(df, formatted_rows, justification, col_widths)
+    print(f"result: {result}")
+    print(f"expected: {expected}")
+
+    assert result == expected
