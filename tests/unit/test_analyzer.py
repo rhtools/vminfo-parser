3d9c8eeaf53d11058e3f353a99ca6def5e360fbd
Jason Ross <jaross@redhat.com>
2024-10-28 13:05:28 -0500

    test(analyzer): unit test for get_unique_os_names

--- tests/unit/test_analyzer.py
+++ tests/unit/test_analyzer.py
@@ -23,3 +23,31 @@ def test_by_os(analyzer: Analyzer, mocker: MockFixture, os_names: list[str]) -> 
     mock_unique_os.assert_called_once()
     assert test_func.call_count == len(os_names)
     test_func.assert_has_calls([((os_name,), {}) for os_name in os_names])
+
+
+@pytest.mark.parametrize(
+    "df_data,os_name,expected",
+    [
+        ({"OS Name": ["os1", "os2"]}, "os1", ["os1"]),
+        ({"OS Name": ["os1", "os2"]}, None, ["os1", "os2"]),
+        ({"OS Name": ["os1", "os2", ""]}, None, ["os1", "os2"]),
+        ({"OS Name": ["os1", "os2", None]}, None, ["os1", "os2"]),
+        ({"OS Name": ["os1", "os2"]}, "os3", []),
+        ({"OS Name": ["", None]}, None, []),
+    ],
+    ids=[
+        "os_name_filter",
+        "no_os_name_filter",
+        "empty_os_name_field",
+        "null_os_name_field",
+        "os_name_filter_not_in_data",
+        "all_invalid_os_names",
+    ],
+)
+def test_get_unique_os_names(analyzer: Analyzer, df_data: dict, os_name: str | None, expected: list[str]) -> None:
+    analyzer.vm_data.df = pd.DataFrame(data=df_data)
+    analyzer.config.os_name = os_name
+
+    response = analyzer.get_unique_os_names()
+
+    assert response == expected
